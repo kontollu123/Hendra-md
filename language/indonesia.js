c8558f7cdc140e7eb4a9d66daff9808aa73ca819
kontollu123 <109311309+kontollu123@users.noreply.github.com>
2022-07-22 05:57:56 +0700

    Add files via upload

--- language/indonesia.js
+++ language/indonesia.js
@@ -1559,8 +1559,8 @@ Goodbye`
 exports.source = () =>{
 return`*------「 SOURCE CODE 」 ------*
 
-Base : https://github.com/DikaArdnt/Hisoka-Morou
-Recode : https://youtube.com/playlist?list=PLFCglBzFPHG7vSQaa9S3P8yfla_d9w-2-
+Base : 
+Recode :-
 `
 }
 exports.tos = (ownernomer) => {
